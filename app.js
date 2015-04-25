ca5b8a4ab795433d68e08610ad3af5c22e022eda
Aaron Sawyer <me@aarondevon.com>
2015-04-25 13:25:49 -0500

    Added buttons 2-10, generate button, and class array

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 /**
  * Created by aaronsawyer on 4/25/15.
  */
+
+var people = ["Erik", "Aaron", "Alicia", "Casie", "Clare", "Cody", "Jeanne", "Kaitlin", "Kelly", "Luke", "Mary", "Michael", "Michelle", "Rom", "Steve", "Terry", "Tracy", "Vince", "Brian", "Chelsea"];
+
